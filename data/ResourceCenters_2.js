a916d3fcb39291a7a9f940b963b928c99db038ec
suzannedraper <75545240+suzannedraper@users.noreply.github.com>
2020-12-05 15:33:02 -0500

    Add files via upload

---
+++ data/ResourceCenters_2.js
@@ -0,0 +1,9 @@
+var json_ResourceCenters_2 = {
+"type": "FeatureCollection",
+"name": "ResourceCenters_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "COMPLETE_A": "946 N Mills Avenue", "ZIPCODE": "32803", "DOR_SHORT_": "INST-CHARITABLE", "LATITUDE": 28.55812, "LONGITUDE": -81.36463, "layer": "The Center", "CHRCTRSTCS": "STI Testing, HIV Testing + Counseling, Hepatitis C Testing, Counseling, PrEP Resources, Group Programming, and more. http:\/\/www.thecenterorlando.org\/" }, "geometry": { "type": "Point", "coordinates": [ -81.364624829213739, 28.558121603980542 ] } },
+{ "type": "Feature", "properties": { "COMPLETE_A": "911 N Mills Avenue", "ZIPCODE": "32803", "DOR_SHORT_": "STORE\/OFFICE\/RES I", "LATITUDE": 28.55716, "LONGITUDE": -81.36429, "layer": "Zebra Coalition", "CHRCTRSTCS": "Network of organizations which provide services to LGBTQIA+ youth ages 13-24. https:\/\/zebrayouth.org\/" }, "geometry": { "type": "Point", "coordinates": [ -81.36428820864171, 28.557162641892443 ] } }
+]
+}
